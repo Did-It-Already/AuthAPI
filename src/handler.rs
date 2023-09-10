72087734245a06976c7d9b7d79cbcd969891be66
Daniela Tocua <danidatope@gmail.com>
2023-09-10 18:35:17 -0500

    basic jwt auth

---
+++ src/handler.rs
@@ -0,0 +1,148 @@
+use crate::{
+    jwt_auth,
+    model::{LoginUserSchema, RegisterUserSchema, TokenClaims, User},
+    response::FilteredUser,
+    AppState,
+};
+use actix_web::{
+    get, post, web, HttpMessage, HttpRequest, HttpResponse, Responder,
+};
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+use chrono::{prelude::*, Duration};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde_json::json;
+use sqlx::Row;
+
+
+#[post("/auth/register")]
+async fn register_user_handler(
+    body: web::Json<RegisterUserSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let exists: bool = sqlx::query("SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)")
+        .bind(body.email.to_owned())
+        .fetch_one(&data.db)
+        .await
+        .unwrap()
+        .get(0);
+
+    if exists {
+        return HttpResponse::Conflict().json(
+            serde_json::json!({"status": "fail","message": "User with that email already exists"}),
+        );
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(body.password.as_bytes(), &salt)
+        .expect("Error while hashing password")
+        .to_string();
+    let query_result = sqlx::query_as!(
+        User,
+        "INSERT INTO users (email,password) VALUES ($1, $2) RETURNING *",
+        body.email.to_string().to_lowercase(),
+        hashed_password
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result {
+        Ok(user) => {
+            let user_response = serde_json::json!({"status": "success","data": serde_json::json!({
+                "user": filter_user_record(&user)
+            })});
+
+            return HttpResponse::Ok().json(user_response);
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .json(serde_json::json!({"status": "error","message": format!("{:?}", e)}));
+        }
+    }
+}
+#[post("/auth/login")]
+async fn login_user_handler(
+    body: web::Json<LoginUserSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let query_result = sqlx::query_as!(User, "SELECT * FROM users WHERE email = $1", body.email)
+        .fetch_optional(&data.db)
+        .await
+        .unwrap();
+
+    let is_valid = query_result.to_owned().map_or(false, |user| {
+        let parsed_hash = PasswordHash::new(&user.password).unwrap();
+        Argon2::default()
+            .verify_password(body.password.as_bytes(), &parsed_hash)
+            .map_or(false, |_| true)
+    });
+
+    if !is_valid {
+        return HttpResponse::BadRequest()
+            .json(json!({"status": "fail", "message": "Invalid email or password"}));
+    }
+
+    let user = query_result.unwrap();
+
+    let now = Utc::now();
+    let iat = now.timestamp() as usize;
+    let exp = (now + Duration::minutes(60)).timestamp() as usize;
+    let claims: TokenClaims = TokenClaims {
+        sub: user.id.to_string(),
+        exp,
+        iat,
+    };
+
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(data.env.jwt_secret.as_ref()),
+    )
+    .unwrap();
+
+    HttpResponse::Ok()
+        .json(json!({"status": "success", "token": token}))
+}
+
+
+#[get("/auth/check")]
+async fn check_token_handler(
+    req: HttpRequest,
+    data: web::Data<AppState>,
+    _: jwt_auth::JwtMiddleware,
+) -> impl Responder {
+    let ext = req.extensions();
+    let user_id = ext.get::<uuid::Uuid>().unwrap();
+
+    let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", user_id)
+        .fetch_one(&data.db)
+        .await
+        .unwrap();
+
+    let json_response = serde_json::json!({
+        "status":  "success",
+        "data": serde_json::json!({
+            "user": filter_user_record(&user)
+        })
+    });
+
+    HttpResponse::Ok().json(json_response)
+}
+
+fn filter_user_record(user: &User) -> FilteredUser {
+    FilteredUser {
+        id: user.id.to_string(),
+        email: user.email.to_owned(),
+    }
+}
+
+pub fn config(conf: &mut web::ServiceConfig) {
+    let scope = web::scope("/api")
+        .service(register_user_handler)
+        .service(login_user_handler)
+        .service(check_token_handler);
+    conf.service(scope);
+}
